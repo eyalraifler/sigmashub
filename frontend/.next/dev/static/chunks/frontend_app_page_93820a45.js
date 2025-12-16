d2dc25302dc9099829315edeeac7ff2ca15b180b
eyalr <eyalraifler3@gmail.com>
2025-12-16 20:43:13 +0200

    Ignore node_modules and remove it from repo

--- frontend/.next/dev/static/chunks/frontend_app_page_93820a45.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f8c414b9._.js",
-  "static/chunks/[root-of-the-server]__365c0750._.js"
-],
-    source: "dynamic"
-});
