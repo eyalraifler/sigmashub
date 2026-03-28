94f3f9f35a7f917f8467b31d0cf7f0d2b03be47b
eyalraifler <eyalraifler3@gmail.com>
2026-03-28 04:15:26 +0300

    add start tour button

--- frontend/app/app/layout.js
+++ frontend/app/app/layout.js
@@ -17,6 +17,7 @@ export default async function AppLayout({ children }) {
       <Sidebar username={username} userId={userId ? Number(userId) : null} />
       {children}
       <AppTourWrapper initialRun={!tourCompleted} />
+      <TourFloatButton />
     </main>
   );
 }
