d8747fd4fb362c96248cb4b1b8046350cdb200d0
eyalraifler <eyalraifler3@gmail.com>
2026-04-13 18:27:58 +0300

    feature: support cloudeslared tunnel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: `${process.env.INTERNAL_API_URL || "http://localhost:8000"}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
